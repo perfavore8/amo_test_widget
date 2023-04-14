c6a43fc0d69328aee71b9bce6165974a317ea4c0
perfavore8 <nekit2353@mail.ru>
2023-04-14 15:39:02 +0200

    new features

---
+++ src/components/BtnsSaveClose.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="btns">
+    <button class="sls_btn btn_grey" @click="close()" v-if="show_close">
+      <slot name="close">Отмена</slot>
+    </button>
+    <slot name="other_btns"></slot>
+    <button
+      class="sls_btn btn_blue"
+      @click="save()"
+      v-if="show_save"
+      :disabled="disabledSave"
+    >
+      <slot name="save">Сохранить</slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    show_close: {
+      type: Boolean,
+      required: false,
+      default() {
+        return true;
+      },
+    },
+    show_save: {
+      type: Boolean,
+      required: false,
+      default() {
+        return true;
+      },
+    },
+    disabledSave: {
+      type: Boolean,
+      required: false,
+      default() {
+        return false;
+      },
+    },
+  },
+  emits: { close: null, save: null },
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+    save() {
+      this.$emit("save");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/app.scss";
+.btns {
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+}
+</style>
